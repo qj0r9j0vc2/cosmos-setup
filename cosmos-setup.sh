58622b98b18b94c7d5763b473fd6a30b95d5cf26
jinu <jinwoo794533@gmail.com>
2023-04-14 13:19:02 +0900

    update shell script

--- cosmos-setup.sh
+++ cosmos-setup.sh
@@ -8,10 +8,3 @@ git clone https://github.com/cosmos/cosmos-sdk --branch v0.47.0
 cd cosmos-sdk && sudo make build
 
 sudo mv build/simd /usr/local/bin
-
-/usr/local/bin/simd init demo --chain-id my-test-chain
-
-jq '.chain_id = "testing"' ~/.simapp/config/genesis.json > temp.json && mv temp.json ~/.simapp/config/genesis.json
-sed -i '/\[api\]/,+3 s/enable = false/enable = true/' ~/.simapp/config/app.toml
-jq '.app_state.gov.voting_params.voting_period = "600s"' ~/.simapp/config/genesis.json > temp.json && mv temp.json ~/.simapp/config/genesis.json
-jq '.app_state.mint.minter.inflation = "0.300000000000000000"' ~/.simapp/config/genesis.json > temp.json && mv temp.json ~/.simapp/config/genesis.json
