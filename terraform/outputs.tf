f8f7e9d5f77a07583ba1f9943f7491e0acb757f5
jinu <jinwoo794533@gmail.com>
2023-04-16 21:35:20 +0900

    rollback

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,3 @@
 output "ec2_connect" {
-  value = "EC2-1: ssh ubuntu@${aws_instance.example-ec2-1.public_ip}\nEC2-1: ssh ubuntu@${aws_instance.example-ec2-2.public_ip}"
+  value = "EC2-1: ssh ubuntu@${aws_instance.example-ec2-1.public_ip}"
 }
