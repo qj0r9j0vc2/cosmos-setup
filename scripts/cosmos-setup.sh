8b57259809e9b62c5f0ca15835b7e3abccf7d054
jinu <jinwoo794533@gmail.com>
2023-04-17 11:49:22 +0900

    fix

--- scripts/cosmos-setup.sh
+++ scripts/cosmos-setup.sh
@@ -3,12 +3,7 @@ sudo snap install go --channel=1.18/stable --classic
 sudo apt update -y
 sudo apt install make gcc g++ jq sed -y
 
-#repository
-<<<<<<< HEAD:scripts/cosmos-setup.sh
-git clone https://github.com/cosmos/cosmos-sdk --branch v0.47.0
-=======
 git clone https://github.com/cosmos/cosmos-sdk --branch v0.47.1
->>>>>>> test:cosmos-setup.sh
 cd cosmos-sdk && sudo make build
 
 sudo mv build/simd /usr/local/bin
