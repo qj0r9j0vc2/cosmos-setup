865b34f025f14c307ae0874d4fce19349d35a6b1
jinu <jinwoo794533@gmail.com>
2023-04-26 23:28:31 +0900

    remove ansible

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -28,9 +28,31 @@ resource "aws_instance" "example-ec2-1" {
       private_key = file("~/.ssh/id_rsa")
     }
   }
+}
 
-  provisioner "local-exec" {
-    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ubuntu -i '${aws_instance.example-ec2-1.public_ip},' --private-key ~/.ssh/id_rsa -e 'pub_key=~/.ssh/id_rsa.pub' ../ansible/execute-script.yml"
+resource "aws_instance" "example-ec2-2" {
+  ami = "ami-0c6e5afdd23291f73" # Canonical, Ubuntu, 20.04 LTS, amd64 focal image build on 2023-03-28
+  instance_type = "t3.small"
+  subnet_id = aws_subnet.example-subnet-publicA.id
+  security_groups = [
+    aws_security_group.example-sg-ssh.id,
+    aws_default_security_group.default.id,
+    aws_security_group.exampmle-sg-simapp.id
+  ]
+  key_name = "example-key-pair"
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "example-ec2-2"
   }
+  provisioner "remote-exec" {
+    inline = ["sudo apt update", "sudo apt install python3 -y", "echo Done!"]
 
+    connection {
+      host        = aws_instance.example-ec2-1.public_ip
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file("~/.ssh/id_rsa")
+    }
+  }
 }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,6 @@
-output "ec2_connect" {
-  value = "EC2-1: ssh ubuntu@${aws_instance.example-ec2-1.public_ip}"
+output "ec2_connect_1" {
+  value = "EC2-1: \nscp ../scripts/cosmos-setup.sh ubuntu@${aws_instance.example-ec2-1.public_ip}:~/\nssh ubuntu@${aws_instance.example-ec2-1.public_ip}\n"
+}
+output "ec2_connect_2" {
+  value = "EC2-1: \nscp ../scripts/cosmos-setup.sh ubuntu@${aws_instance.example-ec2-2.public_ip}:~/\nssh ubuntu@${aws_instance.example-ec2-2.public_ip}\n"
 }
